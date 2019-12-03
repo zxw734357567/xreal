13998be5bb74b31a1f28cb4cd8c5d3bd19f4d0c8
zhangxiaowu <734357567@qq.com>
2019-12-03 10:15:30 +0800

    Initial commit

---
+++ src/main/java/com/tmd/xreal/XrealApplication.java
@@ -0,0 +1,13 @@
+package com.tmd.xreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XrealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XrealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmd/xreal/XrealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmd.xreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XrealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
