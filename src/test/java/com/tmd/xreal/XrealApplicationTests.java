7fb6090ec79aaabed17f00b829d52918cc8743a9
zhangxiaowu <734357567@qq.com>
2019-12-14 16:57:10 +0800

    druid加入

--- src/test/java/com/tmd/xreal/XrealApplicationTests.java
+++ src/test/java/com/tmd/xreal/XrealApplicationTests.java
@@ -1,13 +1,44 @@
 package com.tmd.xreal;
 
-import org.junit.jupiter.api.Test;
+import org.junit.*;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class XrealApplicationTests {
+@RunWith(SpringRunner.class)
+public class XrealApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @BeforeClass
+    public  static   void setUpClas()throws Exception{
+        System.out.println("this is  BeforeClass");
     }
 
+    //=============准备
+    @Before
+    public void setUp() throws Exception{
+        System.out.println("哈哈哈");
+    }
+
+
+
+
+
+
+
+    @Test
+   public void contextLoads() throws Exception{
+        System.out.println("正式测试");
+    }
+
+    //=============销毁
+    @After
+    public void tearUp()throws Exception{
+        System.out.println("this is after");
+    }
+
+    @AfterClass
+    public  static void tearUpStatic()throws Exception{
+        System.out.println("this is AfterClass");
+    }
 }
